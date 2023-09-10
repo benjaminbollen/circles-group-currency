18ce683c75cbb47de98a5b2b745f54670a4f9cc6
jaensen <4954577+jaensen@users.noreply.github.com>
2023-09-10 02:43:31 +0200

    commented the test cases

--- test/GroupCurrencyTokenTest.sol
+++ test/GroupCurrencyTokenTest.sol
@@ -48,6 +48,10 @@ contract GroupCurrencyTokenTest is Test {
         }
     }
 
+
+    // This test validates the temporary suspension of the minting functionality.
+    // Initially, it sets up a GroupCurrencyToken (GCT) and performs minting operations, after which it temporarily suspends minting.
+    // The test then verifies that minting fails during the suspension period and succeeds again after resuming normal operations.
     function testSuspendTemporarily() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         GroupTokenAndDiscriminator gct = user.createSingleMemberGroupCurrency(MintingMode.EveryoneCanMint, user.getUserAddress(), true, 0);
@@ -73,6 +77,9 @@ contract GroupCurrencyTokenTest is Test {
         user.gctMint(gct.groupToken(), user.circlesToken(), 50);
     }
 
+    // This test validates the permanent suspension of the minting functionality.
+    // It initially sets up a GCT and mints tokens.
+    // It then permanently suspends minting and confirms that minting fails, and no changes can be made to the minting mode thereafter.
     function testSuspendPermanently() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         GroupTokenAndDiscriminator gct = user.createSingleMemberGroupCurrency(MintingMode.EveryoneCanMint, user.getUserAddress(), true, 0);
@@ -99,6 +106,7 @@ contract GroupCurrencyTokenTest is Test {
         }
     }
 
+    // This test confirms that the DiscriminatorChanged event is correctly emitted when the discriminator associated with a group token is altered.
     function testEmitDiscriminatorChanged() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         GroupTokenAndDiscriminator gct = user.createEmptyGroupCurrency(MintingMode.EveryoneCanMint, 0);
@@ -109,6 +117,8 @@ contract GroupCurrencyTokenTest is Test {
         user.gctChangeDiscriminator(gct.groupToken(), gct.groupTokenOwner().getUserAddress());
     }
 
+    // This test verifies that the minting fee is correctly applied.
+    // It confirms that the minted amount is reduced by the appropriate fee and that the Minted event correctly reflects the fee applied.
     function testMintFee() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         GroupTokenAndDiscriminator gct = user.createSingleMemberGroupCurrency(MintingMode.EveryoneCanMint, user.getUserAddress(), true, 100);
@@ -123,6 +133,8 @@ contract GroupCurrencyTokenTest is Test {
         user.gctMint(gct.groupToken(), user.circlesToken(), 50);
     }
 
+    // This test verifies the system's response to minting attempts with untrusted collateral.
+    // Initially, it shows a failed minting attempt due to untrusted collateral, followed by the establishment of trust and a successful minting operation.
     function testMintWithUntrustedCollateral() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         GroupTokenAndDiscriminator gct = user.createEmptyGroupCurrency(MintingMode.EveryoneCanMint, 0);
@@ -145,6 +157,9 @@ contract GroupCurrencyTokenTest is Test {
         user.gctMint(gct.groupToken(), user.circlesToken(), 50);
     }
 
+    // This test validates the functionality where only the owner can mint tokens.
+    // It first changes the minting mode to "OnlyOwnerCanMint" and verifies that other users can't mint.
+    // It then demonstrates that only the owner can successfully mint tokens, even if other users are added to a trust relationship or the group.
     function testOnlyOwnerCanMint() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         MockUser otherUser = mockEnv.signup(50000000000000000000);
@@ -182,6 +197,9 @@ contract GroupCurrencyTokenTest is Test {
         }
     }
 
+    // This test validates the scenario where only members can mint tokens.
+    // It checks the minting restrictions by trying to mint as a non-member, followed by successful minting operations as a member.
+    // It also tests the removal of members and the consequent minting restrictions.
     function testOnlyMemberCanMint() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         MockUser otherUser = mockEnv.signup(50000000000000000000);
@@ -212,6 +230,8 @@ contract GroupCurrencyTokenTest is Test {
         user.discriminatorRemoveMember(gct.discriminator(), user.getUserAddress());
     }
 
+    // This test confirms that in the "EveryoneCanMint" mode, any user can mint tokens using trusted collateral.
+    // It tests the minting operations using trusted and untrusted collateral, and after changing minting modes, confirming that the rules apply correctly in each scenario.
     function testEveryoneCanMint() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         MockUser otherUser = mockEnv.signup(50000000000000000000);
@@ -257,6 +277,8 @@ contract GroupCurrencyTokenTest is Test {
         otherUser.gctMint(gct.groupToken(), user.circlesToken(), 50);
     }
 
+    // This function tests adding a member to a Group Token And Discriminator (GCT).
+    // It verifies that only trusted users can mint tokens and members must be added first before they can mint tokens.
     function testAddMember() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         GroupTokenAndDiscriminator gct = user.createSingleMemberGroupCurrency(MintingMode.OnlyMembersCanMint, user.getUserAddress(), false, 0);
@@ -293,6 +315,9 @@ contract GroupCurrencyTokenTest is Test {
         }
     }
 
+    // This function tests removing a member from a GCT.
+    // It confirms that members can remove themselves or be removed by the owner even if they are still accepted by the discriminator.
+    // It handles errors for invalid address input and unauthorized removal attempts.
     function testRemoveMember() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         MockUser anyUser = mockEnv.signup(50000000000000000000);
@@ -329,6 +354,8 @@ contract GroupCurrencyTokenTest is Test {
         }
     }
 
+    // This function tests burning tokens in a GCT.
+    // It mints some tokens first and then burns them, checking for the correct events to be emitted.
     function testBurnGCT() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         GroupTokenAndDiscriminator gct = user.createSingleMemberGroupCurrency(MintingMode.OnlyMembersCanMint, user.getUserAddress(), true, 0);
@@ -342,6 +369,8 @@ contract GroupCurrencyTokenTest is Test {
         user.gctBurn(gct.groupToken(), 50);
     }
 
+    // This function tests transferring GCT tokens between users.
+    // It mints some tokens and then transfers them to another user, ensuring the correct events are emitted.
     function testTransferGCT() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         MockUser otherUser = mockEnv.signup(50000000000000000000);
@@ -356,6 +385,8 @@ contract GroupCurrencyTokenTest is Test {
         user.gctTransfer(gct.groupToken(), otherUser.getUserAddress(), 50);
     }
 
+    // This function tests if a member can remove themselves from a GCT.
+    // It mints some tokens, removes the user from the GCT, and then tries to mint tokens again, which should fail.
     function testMemberCanRemoveThemselves() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         MockUser otherUser = mockEnv.signup(50000000000000000000);
@@ -377,6 +408,8 @@ contract GroupCurrencyTokenTest is Test {
         }
     }
 
+    // This function tests if the owner can remove any member from a GCT.
+    // It mints some tokens, removes a member using the owner account, and then attempts to mint tokens again with the removed member account, which should fail.
     function testOwnerCanRemoveAnyMember() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         MockUser anyMember = mockEnv.signup(50000000000000000000);
@@ -401,6 +434,8 @@ contract GroupCurrencyTokenTest is Test {
         }
     }
 
+    // This function tests removing trust for denied members in a GCT.
+    // It confirms that any user can remove trust for members who are denied by the discriminator, thereby removing them from the GCT, and making them unable to mint tokens.
     function testAnybodyCanRemoveTrustForDeniedMembers() external {
         MockUser user = mockEnv.signup(50000000000000000000);
         MockUser otherUser = mockEnv.signup(50000000000000000000);
